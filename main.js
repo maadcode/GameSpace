9e0609a18cf0c4f4b78b1eaf2c060e9328ce0dec
davilamanuel <manueldavila.developer@gmail.com>
2020-02-26 20:49:54 -0500

    Add js to game

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-window.addEventListener('load', init)
-
-function init() {
-    var canvas = document.getElementById('#canvas')
+// Objetos importantes del canvas
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+// Crear objeto nave
+var nave = {
+    x: 100,
+    y: canvas.height - 100,
+    width: 50,
+    height: 50
 }
+// Definir variables para imagenes
+var fondo
+// Definir funciones
+function loadMedia() {
+    fondo = new Image()
+    fondo.src = './assets/space.jpg'
+    fondo.onload = function() {
+        var intervalo = window.setInterval(frameLoop, 1000/55)
+    }
+}
+
+function drawBackground() {
+    ctx.drawImage(fondo, 0, 0)
+}
+
+function drawSpaceShip() {
+    ctx.save()
+    ctx.fillStyle = 'white'
+    ctx.fillRect(nave.x, nave.y, nave.width, nave.height)
+    ctx.restore()
+}
+
+function frameLoop() {
+    drawBackground()
+    drawSpaceShip()
+}
+
+// Ejecucion de funciones
+loadMedia()
